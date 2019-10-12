b67131f5911418646d2b1f9ebf5fbf6127d825d6
Hongce Zhang <hongcez@princeton.edu>
2019-10-12 22:29:24 +0200

    solvers

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cwd = `pwd`
+unzip yosys.zip -d ~/
+unzip z3.zip -d ~/
+unzip boolector.zip -d ~/
+unzip cosaEnv.zip -d ~/
+unzip CoSA.zip -d ~/
+unzip cvc4.zip -d ~/
+unzip grain.zip -d ~/
+cd ~/yosys
+echo 'install yosys...'
+sudo make install
+cd ~/z3
+sudo 'install z3...'
+sudo make install
+
+
