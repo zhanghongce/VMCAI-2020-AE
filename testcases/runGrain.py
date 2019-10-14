20cfe4d85e8ea425da96f358d91d400eac0ff513
Hongce Zhang <zhanghongce@126.com>
2019-10-14 17:27:36 -0400

    cmd escape

--- testcases/runGrain.py
+++ testcases/runGrain.py
@@ -90,14 +90,14 @@ def RunTests(tests, timeout, total):
           print 'Unable to kill'
         process.wait()
       if directory == 'Pico':
-        cmd = ' '.join(['pgrep','-f','\'.*python.*runGrain.py\''])
-        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, 
+        cmd = ['pgrep','-f','.*python.*runGrain.py']
+        process = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, 
           stderr=subprocess.PIPE)
         my_pid, err = process.communicate()
         should_not_kill = my_pid.splitlines()
         
-        cmd2 = ' '.join(['pgrep', '-n' ,'python'])
-        process2 = subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE, 
+        cmd2 = ['pgrep', '-n' ,'python']
+        process2 = subprocess.Popen(cmd2, shell=False, stdout=subprocess.PIPE, 
           stderr=subprocess.PIPE)
         my_pid, err = process2.communicate()
         should_kill = my_pid.splitlines()
